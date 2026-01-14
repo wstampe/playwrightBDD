0107e1ea876b1e427635b4773edfb7b86cc4aebf
Warren Stampe <wstampe@Mac.net>
2026-01-14 20:46:52 +1100

    (Initial Push fix)

--- features/support/elementHelper.js
+++ features/support/elementHelper.js
@@ -4,8 +4,10 @@ let {dataStore} = require('./dataStore');
 let elementHelper = {
 
     getElementCSS: function getElementCSS(currentElement) {
+
         let elementCSS = pageElements.getPageElements(dataStore.getCurrentPage(), currentElement);
         // console.log('elementCSS: ' + currentElement + ' ==> ' + elementCSS)
         return elementCSS
     },
 };
+module.exports = {elementHelper};
